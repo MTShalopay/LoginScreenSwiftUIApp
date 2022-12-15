d737fee9138a7c66f1661979e0586133a13366fc
Shalopay <shalopay@bk.ru>
2022-12-15 23:04:07 +0300

    Initial Commit

---
+++ LoginScreenSwiftUI/LoginScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenSwiftUIApp.swift
+//  LoginScreenSwiftUI
+//
+//  Created by Shalopay on 15.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
